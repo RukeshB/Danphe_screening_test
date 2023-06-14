1759baa16adba51d07e88d6014069e08b8a13d70
RukeshB <rukeshbasukala@gmail.com>
2023-06-14 18:45:24 +0545

    add another process to solve problem 1: find greatest number

--- greatest_number.rb
+++ greatest_number.rb
@@ -3,11 +3,24 @@
 # numbers.
 
 # method that finds the greatest among the three numbers
+def find_greatest_number(num1, num2, num3)
+  if num1 >= num2 && num2 >= num3
+    num1
+  elsif num2 >= num3 && num2 >= num1
+    num2
+  else
+    num3
+  end
+end
+
+# alternative
 def find_greatest(num1, num2, num3)
   #   create array contaning there parameters as an element of array
   #   then find greatest number from array using max method
   [num1, num2, num3].max
 end
 
-puts "greatest number : #{find_greatest(12, 5, 55)}"
+puts "greatest number : #{find_greatest(12, 55, 55)}"
 puts "greatest number : #{find_greatest(10, 15.5, 15)}"
+puts "greatest number : #{find_greatest_number(12, 55, 55)}"
+puts "greatest number : #{find_greatest_number(10, 15.5, 15)}"
