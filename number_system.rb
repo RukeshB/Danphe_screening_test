90225c5d9f7290495336f581f8ca7840a6de013b
RukeshB <rukeshbasukala@gmail.com>
2023-06-14 18:33:47 +0545

    - add method to convert hex number to Alphabatic notation - add condition when base is 16 - add case for each conversion of number system

--- number_system.rb
+++ number_system.rb
@@ -9,39 +9,111 @@
 # Input: convert_number(42, “decimal”, “binary”)
 # Output: 101010
 
+# method to convert number into another number system
 def convert_number(number, from, to)
-  return to_binary(number, 2) if from == 'decimal' && to == 'binary'
-
-  return to_decimal(number, 2) if from == 'binary' && to == 'decimal'
-
-  'invalid'
+  case [from, to]
+  when %w[decimal binary]
+    decimal_to_other(number, 2)
+  when %w[decimal hex]
+    decimal_to_other(number, 16)
+  when %w[decimal octal]
+    decimal_to_other(number, 8)
+  when %w[binary octal]
+    decimal_number = other_to_decimal(number, 2)
+    decimal_to_other(decimal_number, 8)
+  when %w[binary hex]
+    decimal_number = other_to_decimal(number, 2)
+    decimal_to_other(decimal_number, 16)
+  when %w[octal decimal]
+    other_to_decimal(number, 8)
+  when %w[octal binary]
+    decimal_number = other_to_decimal(number, 8)
+    decimal_to_other(decimal_number, 2)
+  when %w[octal hex]
+    decimal_number = other_to_decimal(number, 8)
+    decimal_to_other(decimal_number, 16)
+  when %w[binary decimal]
+    other_to_decimal(number, 2)
+  when %w[hex decimal]
+    other_to_decimal(number, 16)
+  when %w[hex binary]
+    decimal_number = other_to_decimal(number, 16)
+    decimal_to_other(decimal_number, 2)
+  when %w[hex octal]
+    decimal_number = other_to_decimal(number, 16)
+    decimal_to_other(decimal_number, 8)
+  else
+    'invalid'
+  end
 end
 
 # method to convert number into decimal
-def to_decimal(number, base)
-  numbers = number.to_s.reverse
+def other_to_decimal(number, base)
+  numbers = if base == 16
+              number.to_s.chars.map { |element| hex_alpha_notation(element) }.reverse
+            else
+              number.to_s.reverse.chars
+            end
   result = 0
-  numbers.chars.each_with_index do |num, index|
+  numbers.each_with_index do |num, index|
     result += num.to_i * (base**index)
   end
   result
 end
 
-# method to convert number into binary
-def to_binary(number, base)
+# method to convert decimal number into another number system
+def decimal_to_other(number, base)
   result = ''
   while true
     if number < base
-      result += number.to_s
+      result += if base == 16
+                  hex_alpha_notation(number)
+                else
+                  number.to_s
+                end
       break
     end
     rem = number % base
     number /= base
-    result += rem.to_s
+    result += if base == 16
+                hex_alpha_notation(rem)
+              else
+                rem.to_s
+              end
   end
   result.reverse
 end
 
-# convert_number(42, 'decimal', 'octal')
-puts convert_number(12, 'decimal', 'binary')
-puts convert_number(1100, 'binary', 'decimal')
+# add method that convert hex number to Alphabatic notation
+def hex_alpha_notation(input)
+  input = input.to_s
+  conversions = {
+    '10' => 'A',
+    '11' => 'B',
+    '12' => 'C',
+    '13' => 'D',
+    '14' => 'E',
+    '15' => 'F',
+    'A' => '10',
+    'B' => '11',
+    'C' => '12',
+    'D' => '13',
+    'E' => '14',
+    'F' => '15'
+  }
+
+  conversions[input] || input
+end
+
+puts "decimal 42 = octal #{convert_number(42, 'decimal', 'octal')}"
+puts "decimal 42 = binary #{convert_number(42, 'decimal', 'binary')}"
+puts "decimal 42 = hex #{convert_number(42, 'decimal', 'hex')}"
+puts "octal 12 = binary #{convert_number(12, 'octal', 'binary')}"
+puts "octal 12 = decimal #{convert_number(12, 'octal', 'decimal')}"
+puts "octal 12 = hex #{convert_number(12, 'octal', 'hex')}"
+puts "binary 1100 = decimal #{convert_number(1100, 'binary', 'decimal')}"
+puts "binary 1100 = octal #{convert_number(1100, 'binary', 'octal')}"
+puts "binary 1100 = hex #{convert_number(1100, 'binary', 'hex')}"
+puts "hex C5 = decimal #{convert_number('C5', 'hex', 'decimal')}"
+puts "hex C5 = binary #{convert_number('C5', 'hex', 'binary')}"
+puts "hex C5 = octal #{convert_number('C5', 'hex', 'octal')}"
